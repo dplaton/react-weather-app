53f558eec0096f00142db17cc7578e3098de3bf8
Daniel Platon <platon@adobe.com>
2017-06-16 23:39:53 +0300

    Style the naviagation bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,14 +1,60 @@
 var React = require('react');
-var { Link, IndexLink } = require('react-router');
+var {Link, IndexLink} = require('react-router');
 
-var Nav = (props) => {
-     return(
-            <div>
-                <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-                <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-                <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+var Nav = React.createClass({
+
+    onSearch: function (event) {
+        event.preventDefault();
+        alert('Does not work');
+    },
+
+    render: function () {
+        return (
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li className="menu-text">Weather app</li>
+                        <li>
+                            <IndexLink
+                                to="/"
+                                activeClassName="active"
+                                activeStyle={{
+                                fontWeight: 'bold'
+                            }}>Get Weather</IndexLink>
+                        </li>
+                        <li>
+                            <Link
+                                to="/about"
+                                activeClassName="active"
+                                activeStyle={{
+                                fontWeight: 'bold'
+                            }}>About</Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="/examples"
+                                activeClassName="active"
+                                activeStyle={{
+                                fontWeight: 'bold'
+                            }}>Examples</Link>
+                        </li>
+                    </ul>
+
+                </div>
+                <div className="top-bar-right">
+                    <form onSubmit={this.onSearch}>
+                        <ul className="menu">
+                            <li>
+                                <input type="search" placeholder="Search"/></li>
+                            <li>
+                                <button type="submit" className="button">Get Weather</button>
+                            </li>
+                        </ul>
+                    </form>
+                </div>
             </div>
         )
-}
+    }
+})
 
-module.exports = Nav;
+module.exports = Nav
