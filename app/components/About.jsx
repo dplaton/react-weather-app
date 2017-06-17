08e7086a429f5fbed5d5ec2f40cb809e1c4d3114
Daniel Platon <platon@adobe.com>
2017-06-17 09:06:59 +0300

    Style the About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -13,6 +13,14 @@ var About = React.createClass({
 /* Stateless presentational components can be written like this */
 /* You can only do this with STATELESS components */
 
-var About = (props) => <h3>About component</h3>
+var About = (props) => {
+    return(
+        <div>
+            <h1 className="text-center">About</h1>
+            <p>Weather can be a pain in the ass. It can be your friend, or it can be your enemy. It can add sprinkles and joy to your otherwise sad day, or it can ruin your date. </p>
+            <p>Stay safe. Stay dry. Stay positive. Use our weather app</p>
+        </div>
+    )
+}
 
 module.exports = About;
